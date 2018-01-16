508587504b35eeedb1b5e28e8734caeee9a60aab
jiege <jiege@gmail.com>
2018-01-16 23:38:19 +0800

    封装了一个文件上传工具类，有待后期完善

--- travel1.0/src/main/java/com/java/travel/controller/ExprienceController.java
+++ travel1.0/src/main/java/com/java/travel/controller/ExprienceController.java
@@ -1,27 +1,19 @@
 package com.java.travel.controller;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.java.travel.entity.Exprience;
 
 import com.java.travel.service.ExTypeService;
 import com.java.travel.service.ExprienceService;
+import com.java.travel.util.MyFileUploadUtil;
 
 @Controller
 public class ExprienceController {
@@ -32,9 +24,9 @@ public class ExprienceController {
 
 	/**
 	 * 新增文章
-	 * 
-	 * @param EXTITLE
+	 * @param EXTYPE
 	 * @param exprience
+	 * @param request
 	 * @return
 	 */
 	@RequestMapping(value = "exprienceEdit", method = RequestMethod.POST)
@@ -43,30 +35,15 @@ public class ExprienceController {
 		int exTypeId = exTypeService.selectByName(EXTYPE).getEXTYPEID();
 
 		// 上传封面
-		// 1、将request转为MultipartHttpServletRequest
-		MultipartHttpServletRequest req = (MultipartHttpServletRequest) request;
-		// 2、得到上传文件
-		MultipartFile file = req.getFile("cover");
-		// 得到文件域的名字file.getName() 得到文件名file.getOriginalFilename()
-		// 3、得到目标路径
-		String path = request.getRealPath("/cover") + "\\" + file.getOriginalFilename();
-		// 4、创建目标文件
-		File destFile = new File(path);
-		System.out.println(path);
-		// 5、将文件上传到目标路径
-		try {
-			// 直接使用封装好的 copyInputStreamToFile 实现文件的上传功能
-			FileUtils.copyInputStreamToFile(file.getInputStream(), destFile);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		MyFileUploadUtil uploadUtil = new MyFileUploadUtil(request);		
+		String savePath = uploadUtil.getDefaultTargetPath("cover","/cover/");
+		uploadUtil.upload("cover","/cover/");
 
 		exprience.setEXPRIENCEID(6);
 		exprience.setEXTYPEID(exTypeId);
 		exprience.setEXAUTHORNAME("有梦想的码农");
 		exprience.setEXPUBLISHTIME(new Date());
-		exprience.setEXCOVER(path);
+		exprience.setEXCOVER(savePath);
 
 		exService.insert(exprience);
 
---
+++ travel1.0/src/main/java/com/java/travel/util/MyFileUploadUtil.java
@@ -0,0 +1,130 @@
+package com.java.travel.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+/**
+ * 本文件上传工具类使用说明
+ *  一、各功能独立使用
+ * MyFileUploadUtil uploadUtil = new MyFileUploadUtil(request);
+ * MultipartFile file = uploadUtil.getFile(inputFileName);
+ * String fileName = uploadUtil.getRandomName(file);
+ * String savePath = uploadUtil.getTargetPath(fileDir,fileName);
+ * 1、通过request参数实例化
+ * 2、通过上传文件的控件name：inputFileName获得上传文件
+ * 3、通过获得的文件file获得随机的文件名
+ * 4、通过目标文件夹fileDir和文件名fileName获得保存路径
+ * 5、调用upload(MultipartFile file,String savePath);
+ * 二、用默认的保存方式使用
+ * MyFileUploadUtil uploadUtil = new MyFileUploadUtil(request);		
+ * String savePath = uploadUtil.getDefaultTargetPath(inputFileName,fileDir);
+ * uploadUtil.upload("cover","/cover/");
+ * 1、通过request参数实例化
+ * 2、调用upload(String inputFileName,String fileDir);
+ * 如果需要获取按默认方式保存的地址，则可以调用getDefaultTargetPath(String inputFileName,String fileDir);
+ * @author 刘小杰
+ * 2018年1月16日23:19:42
+ */
+public class MyFileUploadUtil {
+	private HttpServletRequest request;
+
+	public MyFileUploadUtil(HttpServletRequest request) {
+		// TODO Auto-generated constructor stub
+		this.request = request;
+	}
+
+	public MyFileUploadUtil() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 上传图片
+	 * 
+	 * @param savePath 存放路径
+	 */
+	public void upload(MultipartFile file,String savePath) {
+		// 3、得到目标路径(传进来的参数savePath)
+
+		// 4、创建目标文件
+		File destFile = new File(savePath);
+		// 5、将文件上传到目标路径
+		try {
+			// 直接使用封装好的 copyInputStreamToFile 实现文件的上传功能
+			FileUtils.copyInputStreamToFile(file.getInputStream(), destFile);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	/**
+	 * 上传图片方法重载，只要传入以下两个参数就可以
+	 * @param inputFileName 上传文件控件的name
+	 * @param fileDir 目标文件夹
+	 */
+	public void upload(String inputFileName,String fileDir) {
+		MultipartFile file = this.getFile(inputFileName);
+		String fileName = this.getRandomName(file);
+		String savePath = this.getTargetPath(fileDir,fileName);
+		this.upload(file, savePath);
+	}
+	/**
+	 * 获取按默认方式(getRandom方法)命名的的文件路径
+	 * @param inputFileName 上传文件控件的name
+	 * @param fileDir 目标文件夹
+	 * @return
+	 */
+	public String getDefaultTargetPath(String inputFileName,String fileDir) {
+		MultipartFile file = this.getFile(inputFileName);
+		String fileName = this.getRandomName(file);
+		String targetPath = request.getSession().getServletContext().getRealPath(fileDir) + fileName;
+		return targetPath;
+	}
+	
+	/**
+	 * 文件存放目标路径
+	 * 
+	 * @param fileDir 目标文件夹
+	 * @param fileName 存放的文件名称
+	 * @return
+	 */
+	public String getTargetPath(String fileDir, String fileName) {
+		String targetPath = request.getSession().getServletContext().getRealPath(fileDir) + fileName;
+		return targetPath;
+	}
+	
+	/**
+	 * 得到表单上传的文件
+	 * @param inputFileName 上传文件控件的name
+	 * @return
+	 */
+	public MultipartFile getFile(String inputFileName) {
+		// 1、将request转为MultipartHttpServletRequest
+		MultipartHttpServletRequest req = (MultipartHttpServletRequest) request;
+		// 2、得到上传文件
+		MultipartFile file = req.getFile(inputFileName);
+		return file;
+	}
+	
+	/**
+	 * 获得随机的文件名
+	 * @param file
+	 * @return
+	 */
+	public String getRandomName(MultipartFile file) {		
+		//获取原始文件名
+		String preName = file.getOriginalFilename();
+		//获取拓展名
+		preName = preName.substring(preName.lastIndexOf("."));
+		Random random = new Random();
+		String fileName =random.nextInt(10000) + System.currentTimeMillis() + preName;
+		return fileName;
+	}
+}
+
